efcb5d741033ea4efcd910e5f2c9b949ff1a16cf
chohangju <na0173@naver.com>
2021-09-27 16:16:58 +0900

    Feat:socket.io 설치

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let db;
 const MongoClient = require('mongodb').MongoClient
@@ -29,44 +29,8 @@ app.put('/:id', (req, res) => {
 })
 
 app.post('/:id', (req, res) => {
-  res.send(req.body)
-  // db.collection(req.params.id).insertOne(req.body, (err, result) => {
-  //   res.send(result)
-  // })
+  db.collection(req.params.id).insertOne(req.body, (err, result) => {
+    res.send(result)
+  })
 })
 
-const token = '2043330414:AAFWFT1PQ6P0kAmh5331WuuiDRHzXEslsgg';
-const id = '1992525601';
-const message = '안녕'
-app.get(`https://api.telegram.org/bot${token}/sendmessage?chat_id=${id}&text=${message}`)
-
-
-
-
-
-// alarm = {
-//   "거실 적외선 센서": 240,
-//   "방 적외선 센서": 200,
-//   "화장실 적외선 센서": 120,
-//   "거실 인체감지 센서": 100,
-//   "방 인체감지 센서": 60,
-//   "화장실 인체감지 센서": 70
-// }
-
-// th={
-//   time: "11:01",
-//   humidity: 60,
-//   temperature: 26.2
-// }
-
-// sensorValue= {
-//   "현관문 움직임 감지": 1,
-//   "방문 움직임 감지": 0,
-//   "화장문 움직임 감지": 0,
-//   "거실 인체 감지": 1,
-//   "방 인체 감지": 1,
-//   "화장실 인체 감지": 1,
-//   "hour": 20,
-//   "min": 28,
-//   "sec": 54
-// }
